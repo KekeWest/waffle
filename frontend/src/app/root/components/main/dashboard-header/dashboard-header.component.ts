4ac84410f607289a5d0f53932a2d9973d73a087e
kekewest <kekewest@users.noreply.github.com>
2017-06-26 19:21:20 +0900

    update

--- frontend/src/app/root/components/main/dashboard-header/dashboard-header.component.ts
+++ frontend/src/app/root/components/main/dashboard-header/dashboard-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MeStoreService } from "app/common/services";
 
 @Component({
   selector: 'wf-dashboard-header',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private meStoreService: MeStoreService
+  ) { }
 
   ngOnInit() {
   }
